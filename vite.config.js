d8622bcb57420220903e3825065a2b38136875d1
Venkatrajan-R <venkatrajan.rk@gmail.com>
2026-02-23 21:48:12 +0530

    Added base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // or your framework plugin
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: "/venkat-portfolio/",   // 👈 ADD THIS LINE
   plugins: [
     react(),
-    tailwindcss(), // Add this line
+    tailwindcss(),
   ],
 })
